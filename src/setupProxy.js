e957695d9c00b1f0351928710c75c64327947f4e
deso0917 <denis.solokhin@netcracker.com>
2024-07-07 01:49:08 +0300

    Add reverse proxy to avoid CORS issues

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,9 +3,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware({
-      pathFilter: '/api',
+      pathFilter: '/timeapi',
       target: 'https://timeapi.io',
       changeOrigin: true,
+      pathRewrite: {
+        '^/timeapi': '', // Remove the '/api' prefix
+      },
     }),
   );
 };
