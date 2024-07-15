28cd522e9aebe1de0a25d484a3fef330a86028b5
Andrey Zhadanov <a.zhadanov@icloud.com>
2024-07-15 21:22:20 +0300

    potential fix for rotation

--- src/App/pages/StartPage/index.tsx
+++ src/App/pages/StartPage/index.tsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 
 import { EmptyScreen } from '../../components/EmptyScreen';
 import { TEST_ID, TIMER_END } from './constants';
-import { useCountdown, useWindowWidth } from './hooks';
+import { useCountdown, useOrientation } from './hooks';
 import { Outlet } from './Outlet';
 
 import styles from './styles.module.scss';
@@ -11,11 +11,11 @@ import styles from './styles.module.scss';
 export const StartPage: React.FC = () => {
   const { testId } = useParams();
 
-  const windowWidth = useWindowWidth();
+  const angle = useOrientation();
   const timer = useCountdown();
 
   const renderMainScreen = () => {
-    if (windowWidth < 500) {
+    if (angle !== 90) {
       return <div className={styles.middleBlock}>Rotate screen</div>;
     }
 
@@ -33,7 +33,11 @@ export const StartPage: React.FC = () => {
       );
     }
 
-    return <div className={styles.middleBlock}><EmptyScreen /></div>;
+    return (
+      <div className={styles.middleBlock}>
+        <EmptyScreen />
+      </div>
+    );
   };
 
   return <div className={styles.screen}>{renderMainScreen()}</div>;
